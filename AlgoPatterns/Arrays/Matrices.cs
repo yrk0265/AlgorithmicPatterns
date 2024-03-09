6c14a04c4dee7c2b17bb2ca1713657430638fa81
yrk0265 <yrk0265@gmail.com>
2024-03-09 23:35:45 +0530

    1.Modified for matrices,sortcolors.

---
+++ AlgoPatterns/Arrays/Matrices.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProject.Arrays
+{
+    internal class WavePrintMatrices
+    {
+        private int[,] _matrices;
+        public WavePrintMatrices(int[,] matrices)
+        {
+            _matrices = matrices;
+        }
+        public void PrintWaveMatrix()
+        {
+            int m = _matrices.GetLength(0);
+            int n = _matrices.GetLength(1);
+            for (int startCol = 0; startCol < n; startCol++)
+            {
+                //even no of col -> Top to bottom 
+                if ((startCol & 1) == 0)
+                {
+                    for (int i = 0; i < m; i++)
+                    {
+                        Console.Write(_matrices[i, startCol] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    //odd no of col -> Bottom to Top
+                    for (int i = m - 1; i >= 0; i--)
+                    {
+                        Console.Write(_matrices[i, startCol] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ AlgoPatterns/Arrays/SortColors.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProject.Arrays
+{
+    internal class SortColors
+    {
+        private int[] _arr;
+        public SortColors(int[] arr)
+        {
+            _arr = arr;
+        }
+        public void SortColorArray()
+        {
+            int low = 0;
+            int high = _arr.Length - 1;
+            int mid = 0;
+            while (high > mid)
+            {
+                mid = (low + high) / 2;
+                if (_arr[mid] == 0)
+                {
+                    Swap(low, mid, _arr);
+                    low++;
+                    mid++;
+                }
+                else if (_arr[mid] == 1)
+                {
+                    mid++;
+                }
+                else
+                {
+                    Swap(mid, high, _arr);
+                    high--;
+                }
+            }
+        }
+        private void Swap(int lowormid, int highormid, int[] arr)
+        {
+            int temp;
+            temp = arr[lowormid];
+            arr[lowormid] = arr[highormid];
+            arr[highormid] = temp;
+        }
+    }
+}
