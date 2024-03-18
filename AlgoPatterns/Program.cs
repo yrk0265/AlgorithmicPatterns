99862932fd2d9f4e4b8bfbafa5058623b9da9f5c
yrk0265 <yrk0265@gmail.com>
2024-03-18 14:34:42 +0530

    1.Added MergeSort.

--- AlgoPatterns/Program.cs
+++ AlgoPatterns/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AlgorithmProject;
 using AlgorithmProject.Arrays;
-using ReactProject;
+using AlgorithmProject.Sorting;
+using AlgorithmProject.Strings;
+
 
 Console.WriteLine("--------------------------------------------------");
 //NumericFullPyramid numericFullPyramid = new NumericFullPyramid();
@@ -47,6 +49,49 @@ Console.WriteLine("--------------------------------------------------");
 //    };
 //WavePrintMatrices mat = new WavePrintMatrices(matrices);
 //mat.PrintWaveMatrix();
+Console.WriteLine("--------------------------------------------------");
+//int[,] matrices =
+//    {
+//        { 1, 2, 3, 4 },
+//        { 5, 6, 7, 8 },
+//        { 9, 10, 11, 12 }
+//    };
+//MatricesLeftToRight mat = new MatricesLeftToRight(matrices);
+//mat.PrintWaveMatrix();
+//Console.WriteLine("--------------------------------------------------");
+//int[,] matrices =
+//    {
+//        { 1, 2, 3, 4 },
+//        { 5, 6, 7, 8 },
+//        { 9, 10, 11, 12 }
+//    };
+//YMediaLabsMatrices yMediaLabsMatrices =new YMediaLabsMatrices(matrices);
+//yMediaLabsMatrices.Swap();
+Console.WriteLine("--------------------------------------------------");
+int[][] matrices = new int[2][];
+matrices[0] = new int[] { 2, 3 };
+//matrices[1] = new int[] { 5, 6, 7, 8 };
+//matrices[2] = new int[] { 9, 10, 11, 12 };
+//PrintSpiralMatix printSpiralMatix=new PrintSpiralMatix(matrices);
+//printSpiralMatix.PrintSpiral();
+Console.WriteLine("--------------------------------------------------");
+//int[] arr = { 1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9 };
+//MoveAllZeroesToEndArray moveAllZeroesToEndArray = new MoveAllZeroesToEndArray();
+//moveAllZeroesToEndArray.MoveZerosToTheEnd(arr);
+//moveAllZeroesToEndArray.Print(arr);
+Console.WriteLine("--------------------------------------------------");
+//string str = "Ravi had been saying that he had been there";
+//StringRepitition stringRepitition = new StringRepitition();
+//stringRepitition.Repitition(str);
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("--------------------------------------------------");
+//int[] arr = { 12, 11, 13, 5, 6, 7 };
+//Console.WriteLine("Original array");
+//MergeSort.PrintArray(arr);
+//MergeSort mergeSort = new MergeSort(arr);
+//mergeSort.Sort();
+//Console.WriteLine("Sorted array");
+//MergeSort.PrintArray(arr);
 Console.WriteLine("--------------------------------------------------");
 Console.ReadKey();
 
---
+++ AlgoPatterns/Sorting/MergeSort.cs
@@ -0,0 +1,82 @@
+﻿
+
+namespace AlgorithmProject.Sorting
+{
+    public class MergeSort
+    {
+        private int[] _array;
+        public MergeSort(int[] array)
+        {
+            _array = array;
+        }
+        public void Sort()
+        {
+            MergeSortRecursive(_array, 0, _array.Length - 1);
+        }
+        private static void MergeSortRecursive(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+                MergeSortRecursive(arr, left, middle);
+                MergeSortRecursive(arr, middle + 1, right);
+                Merge(arr, left, middle, right);
+            }
+        }
+        private static void Merge(int[] arr, int left, int middle, int right)
+        {
+            int n1 = middle - left + 1;
+            int n2 = right - middle;
+
+            int[] leftArray = new int[n1];
+            int[] rightArray = new int[n2];
+
+            for (int i = 0; i < n1; i++)
+                leftArray[i] = arr[left + i];
+
+            for (int j = 0; j < n2; j++)
+                rightArray[j] = arr[middle + 1 + j];
+
+            int k = left;
+            int leftIndex = 0, rightIndex = 0;
+
+            while (leftIndex < n1 && rightIndex < n2)
+            {
+                if (leftArray[leftIndex] <= rightArray[rightIndex])
+                {
+                    arr[k] = leftArray[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    arr[k] = rightArray[rightIndex];
+                    rightIndex++;
+                }
+                k++;
+            }
+
+            while (leftIndex < n1)
+            {
+                arr[k] = leftArray[leftIndex];
+                leftIndex++;
+                k++;
+            }
+
+            while (rightIndex < n2)
+            {
+                arr[k] = rightArray[rightIndex];
+                rightIndex++;
+                k++;
+            }
+        }
+        public static void PrintArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + "  ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
